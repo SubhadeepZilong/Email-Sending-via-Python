8e17cb5e0e7f056037d7d04522b9387446708e4e
SubhadeepZilong <63146468+SubhadeepZilong@users.noreply.github.com>
2021-12-25 21:05:52 +0530

    code

---
+++ main.py
@@ -0,0 +1,21 @@
+# ---------- Packages -----------
+
+
+import smtplib
+
+
+# ---------- Information -----------
+
+
+smail = input("Enter Sender's Email: ")
+pwd = input("Enter Password: ")
+rmail = input("Enter Reciever's Email: ")
+
+
+# ---------- Execution -----------
+
+
+server = smtplib.SMTP_SSL("smtp.gmail.com",465)
+server.login(smail,pwd)
+server.sendmail(smail,rmail,msg)
+server.quit()
